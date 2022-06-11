f5731a3b830123b060ae8fdeb9bf282e5c54b53b
Wesley <smithw26@wwu.edu>
2022-06-11 11:34:17 -0700

    Multiple word string functionality added.

--- substrings.rb
+++ substrings.rb
@@ -1,10 +1,14 @@
 def substrings(word, dictionary)
     word_count = Hash.new(0)
-    dictionary.each{ |dict_word| word_count[dict_word] += 1 if word.include?(dict_word) }
-    word_count
+    word_array = word.downcase.split()
+    word_array.each do |split_word|
+        dictionary.each{ |dict_word| word_count[dict_word] += 1 if split_word.include?(dict_word) }
+    end
+    word_count.sort.to_h
 end
 
 
 
-dictionary = ["below","down","go","going","horn","below","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
