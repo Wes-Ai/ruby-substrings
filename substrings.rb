c7cd251c41e873d62b31e305d6eb950361bb989c
Wesley <smithw26@wwu.edu>
2022-06-11 11:16:31 -0700

    Substring method working for single input strings.

--- substrings.rb
+++ substrings.rb
@@ -1,8 +1,10 @@
 def substrings(word, dictionary)
-    
+    word_count = Hash.new(0)
+    dictionary.each{ |dict_word| word_count[dict_word] += 1 if word.include?(dict_word) }
+    word_count
 end
 
 
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("below", dictionary)
+dictionary = ["below","down","go","going","horn","below","how","howdy","it","i","low","own","part","partner","sit"]
+p substrings("below", dictionary)
